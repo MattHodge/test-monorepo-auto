a9f4993766341399094ae5612731cae06e793c0f
Matthew Hodgkins <matthodge@gmail.com>
2022-10-12 05:52:53 +0200

    JIRA-123: Do something

--- apps/emitter/main.py
+++ apps/emitter/main.py
@@ -7,7 +7,7 @@ host = "intake"
 port = 9998
 
 url = "http://{}:{}".format(host, port)
-print("Emitter starting and sending metrics to: {}\n".format(url))
+print("Emitter starting and sending metrics to 1: {}\n".format(url))
 while True:
     newConditions = {"Host":"emitter", "MetricName":"emitter.hit.count", "Value": 4}
     params = json.dumps(newConditions).encode('utf8')
