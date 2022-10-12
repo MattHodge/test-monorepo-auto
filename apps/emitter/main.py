465aeff15be95924e07421ad9f88101be4f96261
Matthew Hodgkins <matthodge@gmail.com>
2022-10-12 05:55:18 +0200

    change in branch

--- apps/emitter/main.py
+++ apps/emitter/main.py
@@ -7,7 +7,7 @@ host = "intake"
 port = 9998
 
 url = "http://{}:{}".format(host, port)
-print("Emitter starting and sending metrics to 1: {}\n".format(url))
+print("Emitter starting and sending metrics to 2: {}\n".format(url))
 while True:
     newConditions = {"Host":"emitter", "MetricName":"emitter.hit.count", "Value": 4}
     params = json.dumps(newConditions).encode('utf8')
